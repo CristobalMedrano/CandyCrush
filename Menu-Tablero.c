d89ce62ae0ff51b8db89f177c3db3edd7080d3ae
Cristobal <cristobal.medrano@usach.cl>
2017-03-16 15:23:13 -0300

    Add files via upload

--- Menu-Tablero.c
+++ Menu-Tablero.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+void tablero(int filas, int columnas);
+void menu();
+void abrirArchivo();
+
+int main()
+{
+	// Inicia el programa
+	// Ejecuta menu del programa
+	menu();
+	//Ejecuta Tablero
+	tablero(9,9);
+	return 0;
+}
+
+void tablero(filas, columnas)
+{
+     
+}
+
+void menu()
+{
+    printf("##############################\n");
+	printf("#        Candy Crush         #\n");
+	printf("#            V.00            #\n");
+	printf("# Paradigmas de Programacion #\n");
+	printf("##############################\n");
+}
+
+void abrirArchivo()
+{
+
+}
