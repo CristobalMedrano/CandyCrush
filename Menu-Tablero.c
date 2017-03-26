7c049dfb6907834fd0769da56d24be31c40ff72f
Cristobal <cristobal.medrano@usach.cl>
2017-03-26 00:43:29 -0300

    Add files via upload
    v02
    
    - Menu Completado.

--- Menu-Tablero.c
+++ Menu-Tablero.c
@@ -13,11 +13,22 @@
 #define MENU_PRINCIPAL 1
 #define MENU_SALIR 0
 
+// #define MENU_JUGAR_MODO_PRUEBA
+#define JUGAR_MOVER 1
+#define CREAR_TABLERO 2
+#define GUARDAR_TABLERO 3
+#define CARGAR_TABLERO 4
+#define VERIFICAR_TABLERO 5
+#define VERIFICAR_CARAMELOS 6
+#define MOSTRAR_TABLERO 7
+#define VOLVER 0
+
 // #define MENU_PRINCIPAL
 #define JUGAR 1
 #define JUGAR_MODO_PRUEBA 2
 #define SALIR 0
 
+// #define MENU_SALIR
 #define CONF_SALIR 1
 #define CONF_NO_SALIR 0
 
@@ -26,19 +37,71 @@
 // Cabecera
 
 // Menu Cabecera
+// MenuJugarModoPrueba
+void jugarMover();
+void crearTablero();
+void guardarTablero();
+void cargarTablero();
+void verificarTablero();
+void verificarCaramelos();
+void mostrarTablero();
+
+// MenuPrincipal
 void jugar();
 
-int obtenerOpcionIngresada(int valMin, int valMax);
+// Verificacion - Validacion
+int obtenerOpcionIngresada(int valMin, int valMax, int tipoMenu);
 void seleccionMenu(int tipoMenu);
-void menuSalir();
+
+//Seleccion Menu
 void menuPrincipal();
 void menuJugarModoPrueba();
-void mostrarMenuSalir();
+void menuSalir();
+
+void mostrarMenu(int tipoMenu);
 void mostrarMenuPrincipal();
 void mostrarMenuJugarModoPrueba();
+void mostrarMenuSalir();
+
+// Estructuras
+
+typedef struct
+{
+
+} Board ;
+
+typedef struct
+{
+
+} Game ;
+
+typedef struct
+{
+
+} Params ;
+
+typedef struct
+{
+	int posX;
+	int posY;
+
+} Position ;
+
+typedef struct
+{
+	
+} Candy ;
 
 
 // Funciones
+
+void jugarMover(){}
+void crearTablero(){}
+void guardarTablero(){}
+void cargarTablero(){}
+void verificarTablero(){}
+void verificarCaramelos(){}
+void mostrarTablero(){}
 
 void jugar()
 {
@@ -47,7 +110,7 @@ void jugar()
 	#endif
 }
 
-int obtenerOpcionIngresada(int valMin, int valMax)
+int obtenerOpcionIngresada(int valMin, int valMax, int tipoMenu)
 {
 	int leerOpcion = SIN_INGRESO;
 	do
@@ -57,12 +120,13 @@ int obtenerOpcionIngresada(int valMin, int valMax)
 		{
 			// Obtenemos el contenido del buffer que sea distinto a salto de linea.
 		    while (getchar() != '\n');
-		    printf("\n- ERROR - Ingrese una opcion VALIDA.\n");
+		    printf("\n\n- ERROR - Ingrese una opcion VALIDA.");
+		    mostrarMenu(tipoMenu);
 		    fflush(stdin); // Limpiamos buffer.
 		}
 
 		#ifdef DEBUG
-			printf("Opcion ingresada = %d\n", leerOpcion);
+			printf("Opcion ingresada = %d\n\n", leerOpcion);
 		#endif
 
 	} while (leerOpcion < valMin || leerOpcion > valMax);
@@ -76,27 +140,65 @@ void seleccionMenu(int tipoMenu)
 {
 	switch (tipoMenu)
 	{
-		case MENU_SALIR: menuSalir();
-						 break;
-		case MENU_PRINCIPAL: menuPrincipal();
+		case MENU_PRINCIPAL: mostrarMenu(MENU_PRINCIPAL);
+							 menuPrincipal();					 
 							 break;
-		case MENU_JUGAR_MODO_PRUEBA: menuJugarModoPrueba();
+		case MENU_JUGAR_MODO_PRUEBA: mostrarMenu(MENU_JUGAR_MODO_PRUEBA); 
+									 menuJugarModoPrueba();
 									 break;
+		case MENU_SALIR: mostrarMenu(MENU_SALIR);
+						 menuSalir();
+				 		 break;
+	}
+}
+
+void menuPrincipal()
+{
+	// Muestra el MenuPrincipal.
+	int opcionIngresada = obtenerOpcionIngresada(0, 2, MENU_PRINCIPAL);
+
+	switch (opcionIngresada)
+	{
+		case JUGAR: jugar();
+					break;
+		case JUGAR_MODO_PRUEBA: seleccionMenu(MENU_JUGAR_MODO_PRUEBA);
+								break;
+		case SALIR: seleccionMenu(MENU_SALIR);
+					break;
 	}
 }
 
 void menuJugarModoPrueba()
 {
-	#ifdef DEBUG
-		printf("Estoy jugando en modo de pruebas...\n");
-	#endif
+	
+	int opcionIngresada = obtenerOpcionIngresada(0, 7, MENU_JUGAR_MODO_PRUEBA);
+
+	switch (opcionIngresada)
+	{
+		case JUGAR_MOVER: jugarMover();
+						  break;
+		case CREAR_TABLERO: crearTablero();
+						  	break;
+		case GUARDAR_TABLERO: guardarTablero();
+						  	  break;
+		case CARGAR_TABLERO: cargarTablero();
+						  	 break;
+		case VERIFICAR_TABLERO: verificarTablero();
+						  		break;
+		case VERIFICAR_CARAMELOS: verificarCaramelos();
+						  		  break;
+		case MOSTRAR_TABLERO: mostrarTablero();
+							  break;
+		case VOLVER: seleccionMenu(MENU_PRINCIPAL);
+					 break;
+	}
+
 }
 
 void menuSalir()
 {
 	// Muestra el MenuSalir.
-	mostrarMenuSalir();
-	int opcionIngresada = obtenerOpcionIngresada(0, 1);
+	int opcionIngresada = obtenerOpcionIngresada(0, 1, MENU_SALIR);
 	
 	switch (opcionIngresada)
 	{
@@ -107,28 +209,17 @@ void menuSalir()
 	}
 }
 
-void menuPrincipal()
+void mostrarMenu(int tipoMenu)
 {
-	// Muestra el MenuPrincipal.
-	mostrarMenuPrincipal();
-	int opcionIngresada = obtenerOpcionIngresada(0, 2);
-
-	switch (opcionIngresada)
+	switch (tipoMenu)
 	{
-		case JUGAR: jugar();
-					break;
-		case JUGAR_MODO_PRUEBA: menuJugarModoPrueba();
-								break;
-		case SALIR: seleccionMenu(MENU_SALIR);
-					break;
+		case MENU_PRINCIPAL: mostrarMenuPrincipal();
+							 break;
+		case MENU_JUGAR_MODO_PRUEBA: mostrarMenuJugarModoPrueba();
+							 		 break;
+		case MENU_SALIR: mostrarMenuSalir();
+						 break;
 	}
-}
-
-void mostrarMenuSalir()
-{
-	printf("- Esta seguro que desea salir?, ingrese numero de opcion deseada:\n");
-	printf("1.- Si\n");
-	printf("0.- No\n");
 }
 
 void mostrarMenuPrincipal()
@@ -137,8 +228,7 @@ void mostrarMenuPrincipal()
 	printf("#        Candy Crush         #\n");
 	printf("#            V.02            #\n");
 	printf("# Paradigmas de Programacion #\n");
-	printf("##############################\n");
-	printf("\n");
+	printf("##############################\n\n");
 	printf("Menu del juego, ingrese numero de opcion deseada: \n");
 	printf("1.- Jugar\n");
 	printf("2.- Jugar en modo de Prueba\n");
@@ -158,6 +248,12 @@ void mostrarMenuJugarModoPrueba()
 	printf("0.- Volver al Menu Anterior\n");
 }
 
+void mostrarMenuSalir()
+{
+	printf("- Esta seguro que desea salir?, ingrese numero de opcion deseada:\n");
+	printf("1.- Si\n");
+	printf("0.- No\n");
+}
 
 int main(int argc, char const *argv[])
 {
